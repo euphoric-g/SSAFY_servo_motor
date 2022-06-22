6d2576c281c0ae3f58db6f09c4c5100d419b791c
B635274 <80505833+B635274@users.noreply.github.com>
2022-06-22 22:09:48 +0900

    Update ctl_steering.h

--- src/lib/ctl_steering.h
+++ src/lib/ctl_steering.h
@@ -3,5 +3,10 @@
 #include "PosInfo.h"
 #include "WaypointInfo.h"
 #include "../DrivingInterface/DrivingInfo.h"
+#include <vector>
+#include <cmath>
+#include "cog_waypoints_to_PosInfo.h"
+
+using namespace std;
 
 float ctl_steering(Car::CarStateValues &sensing_info, WaypointInfo waypoint);
